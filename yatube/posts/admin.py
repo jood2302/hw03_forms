3fb848335c8b132112bd5693f4e0412080e44cd1
jood2302 <slavutin.lv@gmail.com>
2021-07-27 22:41:03 +0300

    Спринт 3. Первый пуш

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -15,4 +15,6 @@ class PostAdmin(admin.ModelAdmin):
 # При регистрации модели Post источником конфигурации для неё назначаем
 # класс PostAdmin
 admin.site.register(Post, PostAdmin)
+
+
 admin.site.register(Group)
