51af27ebdd96ba15832041d2ae4d132ebf18d635
jood2302 <slavutin.lv@gmail.com>
2021-07-29 00:44:20 +0300

    Спринт 3. Попытка 3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,7 +5,7 @@ from .models import Group, Post
 
 def index(request,):
     template = 'posts/index.html'
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.all()[:10]
     context = {
         'posts': posts,
         'title': 'Последние обновления на сайте'
