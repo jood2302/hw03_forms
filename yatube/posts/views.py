bb5b9ec0aa407b77eb999a1be4ad1843a9b5a81e
Leonid Slavutin <slavutin.lv@gmail.com>
2021-10-05 00:33:53 +0300

    Второй пуш

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -67,7 +67,7 @@ def post_create(request):
         post = form.save(commit=False)
         post.author = request.user
         post.save()
-        return redirect('posts:profile', username=request.user.username)  
+        return redirect('posts:profile', username=request.user.username)
     return render(request, 'posts/create_post.html', {'form': form})
 
 
